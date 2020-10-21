956b56721ad4dd81cc55925b9577d3c481e6f7ae
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2020-10-21 20:34:10 +0200

    Deployed first smart contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,8 +2,10 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 beforeEach(async () => {
     // Get a list of all accounts
     /* One way to do it
@@ -15,12 +17,17 @@ beforeEach(async () => {
    // Another way
     accounts = await web3.eth.getAccounts();
 
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['INBOX SMART CONTRACT']})
+        .send({ from: accounts[0], gas: '1000000'});
+
     // Use one of those accounts to deploy the contract
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         console.log(accounts);
+        console.log(inbox);
      })
 })
 
