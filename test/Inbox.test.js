1bd3674bde00cb7106860fb164b3fd42c3ef1106
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2020-10-22 15:13:36 +0200

    Tested if the contract has been deployed, Tested the contract get method

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = 'INBOX SMART CONTRACT';
 beforeEach(async () => {
     // Get a list of all accounts
     /* One way to do it
@@ -14,21 +16,26 @@ beforeEach(async () => {
             console.log(fetchedAccounts);
         })
     */
-   // Another way
+    // Another way
     accounts = await web3.eth.getAccounts();
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['INBOX SMART CONTRACT']})
-        .send({ from: accounts[0], gas: '1000000'});
-
+        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+        .send({ from: accounts[0], gas: '1000000' });
+    inbox.setProvider(provider);
     // Use one of those accounts to deploy the contract
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
-        console.log(inbox);
-     })
+        // console.log(accounts);
+        assert.ok(inbox.options.address);
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    })
 })
 
 
